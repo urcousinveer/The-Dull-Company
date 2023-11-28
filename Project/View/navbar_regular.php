aeea10184891f3156909b86182528cbea1285ec6
Kassandra Montgomery <montgomery135@hotmail.com>
2023-11-27 16:09:41 -0800

    added navbar, changed name of browse_product.php

---
+++ Project/View/navbar_regular.php
@@ -0,0 +1,21 @@
+<?php
+//navbar for every user
+
+echo '
+
+<html>
+<head>
+    <link rel="stylesheet" href="../css/navbar.css">
+</head>
+<header>
+    <nav class="navbar">
+        <ul class="navbar-menu">
+            <li><a href="home_page.php">Home</a></li>
+            <li><a href="browse_products.php">Browse Products</a></li>
+            <li><a href="search_products.php">Search</a></li>
+        </ul>
+    <nav>
+<header>
+</html>
+'
+?>
